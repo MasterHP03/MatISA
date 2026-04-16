488768c724c42764333d94b26b048ccfb5212e52
Matsalter <geniusyhp@gmail.com>
2026-04-17 03:36:23 +0900

    feat: support RISC-V ABI name of registers and code comments

--- main.py
+++ main.py
@@ -15,9 +15,27 @@ reg_num = 32
 regs = [0] * reg_num
 reg_map = {
     "zero": 0,
-    "x": 1,
-    "y": 2
+    "ra": 1,
+    "sp": 2,
+    "gp": 3,
+    "tp": 4,
+    "fp": 8,
 }
+
+for i in range(2):
+    reg_map[f"s{i}"] = i + 8 # s0, s1
+
+for i in range(2, 12):
+    reg_map[f"s{i}"] = i + 16 # s2 - s7
+
+for i in range(8):
+    reg_map[f"a{i}"] = i + 10 # a0 - a7
+
+for i in range(3):
+    reg_map[f"t{i}"] = i + 5 # t0 - t2
+
+for i in range(3, 7):
+    reg_map[f"t{i}"] = i + 25 # t3 - t6
 
 mem_size = 1024
 mem = [0] * mem_size
@@ -107,7 +125,7 @@ def encode():
     # 1st pass: Check labels
     curr_line = 1
     for line in lines:
-        line = line.strip()
+        line = re.split(r'[#;]', line)[0].strip()
         if len(line) <= 1: continue
         args = re.split(r'[,\s]+', line)
 
@@ -125,7 +143,7 @@ def encode():
     curr_line = 1
     for line in lines:
         try:
-            line = line.strip()
+            line = re.split(r'[#;]', line)[0].strip()
             if len(line) == 0: continue
             args = re.split(r'[,\s]+', line)
 
